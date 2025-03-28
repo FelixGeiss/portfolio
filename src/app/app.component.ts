ea1219744b1f84bd33e88ed5cc2a69599f8f4f4a
Felix <geissq@gmail.com>
2025-03-28 15:26:29 +0100

    Added SCSS variables and mixins, styled navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
 import { HeroComponent } from "./hero/hero.component";
 import { AboutComponent } from "./about/about.component";
 import { SkillsComponent } from "./skills/skills.component";
@@ -9,11 +8,12 @@ import { ProjectsComponent } from "./projects/projects.component";
 import { TestimonialsComponent } from "./testimonials/testimonials.component";
 import { ContactComponent } from "./contact/contact.component";
 import { FooterComponent } from "./footer/footer.component";
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroComponent, AboutComponent, SkillsComponent, ProjectsComponent, TestimonialsComponent, ContactComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet,HeroComponent, AboutComponent, SkillsComponent, ProjectsComponent, TestimonialsComponent, ContactComponent, FooterComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
