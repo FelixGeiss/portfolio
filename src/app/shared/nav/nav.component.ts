a533613e6e70dcc0ff38c7cfedcd05deee7f30f1
Felix <geissq@gmail.com>
2025-04-16 19:21:26 +0200

    feat: Created mobile design, started navbar, and added burger menu

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss'
+  styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+  // Boolean-Variable, die den Zustand des Menüs speichert
+  menuOpen: boolean = false;
 
+  // Methode zum Wechseln des Zustands
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
 }
+
